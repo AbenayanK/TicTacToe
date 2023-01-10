c79af0adbc74a20947a7ddb64d3a1183ff85b145
Musa <77996753+MusaAqeel@users.noreply.github.com>
2023-01-09 23:13:46 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
+# ICS3U
+# CPT
+# Luciano
+# Tic Tac Toe Game
 
+
+
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Set the window dimensions
+window_width = 400
+window_height = 400
+
+# creating windowb 
+window = pygame.display.set_mode((window_width, window_height))
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Set  background color
+bg_color = WHITE
+
+# Drawing horizontal line across the middle of the window
+pygame.draw.line(window, BLACK, (0, window_height / 2), (window_width, window_height / 2), 5)
